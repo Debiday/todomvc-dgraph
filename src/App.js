9027521283bfa6c09427c868abf7ad9472518f25
Deborah Ong <deborah.oyt@gmail.com>
2021-10-22 11:56:23 -0700

    add to index.html

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import 'todomvc-app-css/index.css'
 
 function App() {
   return (
-    <div className="App">
-    </div>
-  );
+    <div>
+        <header className='todoapp'>
+            <h1>todos</h1>
+            <input
+                className='new-todo'
+                placeholder='What needs to be done?'
+                autoFocus={true}
+            />
+        </header>
+  </div>
+)
 }
+
 
 export default App;
